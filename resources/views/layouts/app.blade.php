1c24894e35f929564bd1d626316a66a901376eb5
alex2000d <demarcoalessandro777@gmail.com>
2024-09-25 16:42:18 +0200

    [UPD] created the controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,21 +8,7 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    <header>
-        <div class="container-fluid bg">
-            <div class="row">
-                <div class="col-6">
-                    <h1 class="mt-2 text-white">Boolfilms</h1>
-                </div>
-                <div class="col-6">
-                   <ul class="list-unstyled gap-3 mt-4 d-flex justify-content-center text-white">
-                      <li><a class="nav-link" href="">home</a></li>
-                      <li><a class="nav-link" href="">movies</a></li>
-                   </ul>
-                </div>
-            </div>
-        </div>
-    </header>
+   @include('partials.header')
     <main>
         @yield('content')
     </main>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="container-fluid bg">
+        <div class="row">
+            <div class="col-6">
+                <h1 class="mt-2 text-white">Boolfilms</h1>
+            </div>
+            <div class="col-6">
+               <ul class="list-unstyled gap-3 mt-4 d-flex justify-content-center text-white">
+                  <li><a class="nav-link" href="">home</a></li>
+                  <li><a class="nav-link" href="">movies</a></li>
+               </ul>
+            </div>
+        </div>
+    </div>
+</header>

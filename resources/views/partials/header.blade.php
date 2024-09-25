59fdc34cb01157cf8b83f69557d6b63caf4efdbf
alex2000d <demarcoalessandro777@gmail.com>
2024-09-25 18:15:09 +0200

    [UPD] created the database

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,7 +6,7 @@
             </div>
             <div class="col-6">
                <ul class="list-unstyled gap-3 mt-4 d-flex justify-content-center text-white">
-                  <li><a class="nav-link" href="">home</a></li>
+                  <li><a class="nav-link" href="{{route('homepage')}}">home</a></li>
                   <li><a class="nav-link" href="">movies</a></li>
                </ul>
             </div>
